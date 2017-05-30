b53a33e6c2e4447dc4ea243d9b3f37ef25f81b95
Nicolas Zelaya <zelayadn@gmail.com>
2017-05-30 19:05:35 -0300

    Adding ts declaration file

---
+++ types/index.d.ts
@@ -0,0 +1,64 @@
+// Declaration file for Node Split Software API SDK v1.0.0
+// Project: https://github.com/splitio/javascript-api
+// Definitions by: Nico Zelaya <https://github.com/NicoZelaya/>
+
+import * as superagent from 'superagent';
+// We import these directly from source, taking advantage of having the src code 
+// on the same package.
+import { ITrafficType as ITrafficTypeFromDto } from '../src/api/dtos/TrafficType';
+import { IAttribute as IAttributeFromDto } from '../src/api/dtos/Attribute';
+import { IEnvironment as IEnvironmentFromDto } from '../src/api/dtos/Environment';
+import { IIdentity as IIdentityFromDto } from '../src/api/dtos/Identity';
+
+declare function getClient (adminKey: string, config?: SplitAPI.IIdentifyConfig): SplitAPI.IdentifyClient;
+
+declare namespace SplitAPI {
+  interface IIdentifyConfig {
+    endpoint?: string;
+    connectionTimeout?: number;
+    debugEnabled?: boolean;
+  }
+
+  class IdentifyConfig implements IIdentifyConfig {
+    private _defaults;
+    readonly endpoint: string;
+    readonly connectionTimeout: number;
+    readonly debugEnabled: boolean;
+    constructor(config?: IIdentifyConfig);
+  }
+
+  class IdentifyClient {
+    readonly config: IIdentifyConfig
+    readonly TrafficType: TrafficTypeClient
+    readonly Attribute: AttributeClient
+    readonly Environment: EnvironmentClient
+    readonly Identity: IdentityClient
+  }
+
+  class TrafficTypeClient {
+    list(): Promise<superagent.Response>
+  }
+
+  class AttributeClient {
+    list(): Promise<superagent.Response>
+    create(attr: IAttribute): Promise<superagent.Response>
+    delete(attr: IAttribute): Promise<superagent.Response>
+  }
+
+  class EnvironmentClient {
+    list(): Promise<superagent.Response>
+  }
+
+  class IdentityClient {
+    save(identity: IIdentity): Promise<superagent.Response>
+    saveBulk(identities: Array<IIdentity>): Promise<superagent.Response>
+    update(identity: IIdentity): Promise<superagent.Response>
+    delete(identity: IIdentity): Promise<superagent.Response>
+  }
+
+  interface IAttribute extends IAttributeFromDto {}
+  interface ITrafficType extends ITrafficTypeFromDto {}  
+  interface IEnvironment extends IEnvironmentFromDto {}  
+  interface IIdentity extends IIdentityFromDto {}  
+}
+
