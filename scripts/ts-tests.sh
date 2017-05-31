79c0212f750e8496eacf91bde0a30ef89d6d8fe0
Nicolas Zelaya <zelayadn@gmail.com>
2017-05-31 16:20:26 -0300

    testing codeship

--- scripts/ts-tests.sh
+++ scripts/ts-tests.sh
@@ -4,7 +4,7 @@ echo -e "Going to TypeScript tests folder and linking the package.\n"
 cd ts-tests ## Go to typescript tests folder
 npm link @splitsoftware/splitio-api ## Link to the cloned code
 echo -e "Installing dependencies for TypeScript declarations testing...\n"
-npm install ## Install dependencies
+#npm install ## Install dependencies
 echo -e "Dependencies installed, running tsc compiler.\n"
 tsc ## Run typescript compiler. No need for flags as we have a tsconfig.json file
 
