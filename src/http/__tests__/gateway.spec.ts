a8814adf76d078a2253cafe5a33e9d13736de3dd
Nicolas Zelaya <zelayadn@gmail.com>
2017-06-20 11:42:38 -0300

    Fixing gateway.spec

--- src/http/__tests__/gateway.spec.ts
+++ src/http/__tests__/gateway.spec.ts
@@ -1,6 +1,7 @@
 import Gateway from '../gateway';
 
 let superagentMock;
+let gatewayInst;
 const fakeAdminKey = 'fake-key';
 const fakeSettings = {
   endpoint: 'mock.split.io'
@@ -24,19 +25,19 @@ beforeAll(() => {
   // We mock superagent
   superagentMock = require('superagent-mock')(superagent, superagentMockConfig);
 
-  this.gateway = new Gateway(fakeAdminKey, fakeSettings);
+  gatewayInst = new Gateway(fakeAdminKey, fakeSettings);
 });
 
 test('Should have the correct API for HTTP methods', () => {
-  expect(typeof this.gateway.get).toBe('function');
-  expect(typeof this.gateway.del).toBe('function');
-  expect(typeof this.gateway.post).toBe('function');
-  expect(typeof this.gateway.put).toBe('function');
-  expect(typeof this.gateway.patch).toBe('function');
+  expect(typeof gatewayInst.get).toBe('function');
+  expect(typeof gatewayInst.del).toBe('function');
+  expect(typeof gatewayInst.post).toBe('function');
+  expect(typeof gatewayInst.put).toBe('function');
+  expect(typeof gatewayInst.patch).toBe('function');
 });
 
 test('Get method should call http library of choice for a GET request to corresponding URL, with corresponding headers and data', () => {
-  return this.gateway.get('/getData').then((res: any) => {
+  return gatewayInst.get('/getData').then((res: any) => {
     const {
       method, parameters, headers, endpoint
     } = res;
@@ -49,7 +50,7 @@ test('Get method should call http library of choice for a GET request to corresp
 });
 
 test('Del method should call http library of choice for a DELETE request to corresponding URL, with corresponding headers and data', () => {
-  return this.gateway.del('/delData').then((res: any) => {
+  return gatewayInst.del('/delData').then((res: any) => {
     const {
       method, parameters, headers, endpoint
     } = res;
@@ -62,7 +63,7 @@ test('Del method should call http library of choice for a DELETE request to corr
 });
 
 test('Post method should call http library of choice for a POST request to corresponding URL, with corresponding headers and data', () => {
-  return this.gateway.post(fakeDto, '/postData').then((res: any) => {
+  return gatewayInst.post(fakeDto, '/postData').then((res: any) => {
     const {
       method, parameters, headers, endpoint
     } = res;
@@ -75,7 +76,7 @@ test('Post method should call http library of choice for a POST request to corre
 });
 
 test('Put method should call http library of choice for a PUT request to corresponding URL, with corresponding headers and data', () => {
-  return this.gateway.put(fakeDto, '/putData').then((res: any) => {
+  return gatewayInst.put(fakeDto, '/putData').then((res: any) => {
     const {
       method, parameters, headers, endpoint
     } = res;
@@ -88,7 +89,7 @@ test('Put method should call http library of choice for a PUT request to corresp
 });
 
 test('Patch method should call http library of choice for a PATCH request to corresponding URL, with corresponding headers and data', () => {
-  return this.gateway.patch(fakeDto, '/patchData').then((res: any) => {
+  return gatewayInst.patch(fakeDto, '/patchData').then((res: any) => {
     const {
       method, parameters, headers, endpoint
     } = res;
@@ -102,7 +103,7 @@ test('Patch method should call http library of choice for a PATCH request to cor
 
 test('If the request fails, returned promise should reject and bring the error', () => {
   // Fixture will throw an error if we call for /forceFailure, does not depend on the method
-  return this.gateway.get('/forceFailure').then(res => {
+  return gatewayInst.get('/forceFailure').then(res => {
     // This should not run, so if it runs we need a failure.
     expect(true).toBeFalsy();
   }, err => {
@@ -113,7 +114,7 @@ test('If the request fails, returned promise should reject and bring the error',
 
 test('rejectedReq method should reject a promise with given error', () => {
   const error = new Error('test');
-  return this.gateway.rejectedReq(error)
+  return gatewayInst.rejectedReq(error)
                 .then(res => expect(res).toBe('this should never happen'))
                 .catch(err => {
                   expect(err).toBeInstanceOf(Error);
@@ -122,7 +123,7 @@ test('rejectedReq method should reject a promise with given error', () => {
 });
 
 test('rejectedReq method should reject a promise with an error with given message', () => {
-  return this.gateway.rejectedReq('test_error_msg')
+  return gatewayInst.rejectedReq('test_error_msg')
                 .then(res => expect(res).toBe('this should never happen'))
                 .catch(err => {
                   expect(err).toBeInstanceOf(Error);
